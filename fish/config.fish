dc9a3d986be0872a70e9a0cbd2a07a3de730c8d2
cartoon-raccoon <shaughn.chan@gmail.com>
2021-03-14 22:12:25 +0800

    Added fish config

---
+++ fish/config.fish
@@ -0,0 +1,48 @@
+set -x EDITOR (which nvim)
+set -x PATH "$HOME/.cargo/bin/:$PATH"
+
+# mpc functions
+function mpcload
+	mpc clear > /dev/null
+	mpc load $argv[1]
+end
+
+function mpcplay
+	mpcload $argv[1] > /dev/null
+	mpc play
+end
+
+# package management functions
+function addpkg
+	sudo pacman -S $argv
+end
+
+function yeet
+	sudo pacman -Rs $argv
+end
+
+function miniyeet
+	sudo pacman -R $argv
+end
+
+function pkgquery
+	pacman -Qi $argv
+end
+
+function pkgcount
+	pacman -Q | wc -l
+end
+
+
+alias fishreload="source ~/.config/fish/config.fish"
+
+alias alacconf="$EDITOR ~/.config/alacritty/alacritty.yml"
+alias fishconf="$EDITOR ~/.config/fish/config.fish"
+alias i3config="$EDITOR ~/.config/i3/config"
+alias polyconf="$EDITOR ~/.config/polybar/config"
+alias xmonadconf="$EDITOR ~/.xmonad/xmonad.hs"
+alias xmobarconf="$EDITOR ~/.config/xmobar/xmobarrc"
+alias spectrconf="$EDITOR ~/.spectrwm.conf"
+alias baraction="$EDITOR ~/.config/bar_action.sh"
+alias cavaconf="$EDITOR ~/.config/cava/config"
+alias picomconf="$EDITOR ~/.config/picom/picom.conf"
