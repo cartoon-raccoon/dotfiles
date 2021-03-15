cfa7d63e011f3f37772865d361547b0ea814e68c
cartoon-raccoon <shaughn.chan@gmail.com>
2021-03-15 20:13:03 +0800

    Added commit script

---
+++ commit.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# commit script to keep the package list up to date.
+
+pacman -Qqet > fullpackagelist
+
+case $1 in
+	-m)
+		git commit -m "$2"
+		;;
+	--amend)
+		git commit --amend
+		;;
+esac
