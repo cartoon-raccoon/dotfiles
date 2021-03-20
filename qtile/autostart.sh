b5d949cdebe75e6f291d4d21816429b1b15411c7
cartoon-raccoon <shaughn.chan@gmail.com>
2021-03-20 16:43:09 +0800

    qtile: removed polybar, configured top bar

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -4,6 +4,6 @@ xautolock -time 5 -locker "/home/sammy/.config/i3/i3lock" &
 picom &
 feh --bg-scale /home/sammy/Pictures/minimalistwp.png
 
-$HOME/.config/polybar/launch.sh &
+#$HOME/.config/polybar/launch.sh &
 
 pkill mpd_notify.sh; ~/.config/mpd/mpd_notify.sh &
