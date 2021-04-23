d392c7e538d4ff954c9773e0c20bed280754b451
cartoon-raccoon <shaughn.chan@gmail.com>
2021-04-23 21:40:18 +0800

    qtile: changed locker to xss-lock

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-xautolock -time 5 -locker "/home/sammy/.config/i3/i3lock" &
+xss-lock "/home/sammy/.config/i3/i3lock" &
 picom &
 feh --bg-scale /home/sammy/Pictures/Wallpaper/streets.jpg
 
+nm-applet &
 #$HOME/.config/polybar/launch.sh &
 
 pkill mpd_notify.sh; ~/.config/mpd/mpd_notify.sh &
