88fccefc84d35625ccfc35455d133df00d56441e
cartoon-raccoon <shaughn.chan@gmail.com>
2022-01-14 01:13:54 +0800

    qtile: added polkit-gnome to autostart

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -7,4 +7,7 @@ feh --bg-scale /home/sammy/Pictures/Wallpaper/dont_fly.png
 nm-applet &
 #$HOME/.config/polybar/launch.sh &
 
+#! NOTE: running polkit authenticator here means it will only work on qtile
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+
 pkill mpd_notify.sh; ~/.config/mpd/mpd_notify.sh &
