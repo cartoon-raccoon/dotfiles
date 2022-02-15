e90be3fda69246c5c0be2610a281f48f88644c93
cartoon-raccoon <shaughn.chan@gmail.com>
2022-02-15 15:28:12 +0800

    qtile: changed wallpaper

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+WALLPAPER_PATH="/home/sammy/Pictures/Wallpaper"
+
 xss-lock "/home/sammy/.config/i3/i3lock" &
 picom &
-feh --bg-scale /home/sammy/Pictures/Wallpaper/dont_fly.png
+feh --bg-scale $WALLPAPER_PATH/starfield.jpg
 
 nm-applet &
 #$HOME/.config/polybar/launch.sh &
