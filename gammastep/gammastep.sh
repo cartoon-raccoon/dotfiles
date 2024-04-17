a37ac139305e5456135e06f25adfe5d2006b3a35
Sam <shaughn.chan@gmail.com>
2024-04-17 08:50:55 -0400

    gammastep: added delay to account for network connection

--- gammastep/gammastep.sh
+++ gammastep/gammastep.sh
@@ -2,6 +2,10 @@
 
 trap "killall gammastep" EXIT
 
+# since we're usually getting started at boot, sleep for 2 seconds for us
+# to get connected to a network
+sleep 2
+
 ping -c 1 -W 50 1.1.1.1 || (notify-send "gammastep: unable to connect to network"; exit)
 
 IFS="," read lat long <<< $(curl --silent https://ipinfo.io | jq .loc | tr -d '"')
