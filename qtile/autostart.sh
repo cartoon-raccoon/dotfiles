a617292b2ad71d0fab4d15b510ba7d0cc5e44a5b
Sam <shaughn.chan@gmail.com>
2024-06-27 17:04:21 +0800

    qtile: updated autostart script for screensaver

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -5,6 +5,8 @@ WALLPAPER_PATH="/home/sammy/Pictures/Wallpaper"
 xss-lock "/home/sammy/.config/i3/i3lock" &
 picom &
 #feh --bg-fill "$WALLPAPER_PATH/$DESKTOP_BKGD"
+xset s on
+xset s 600
 
 nm-applet &
 
