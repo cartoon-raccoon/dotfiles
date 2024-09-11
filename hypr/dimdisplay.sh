b6ed62377da9a06282ffe5f95b5ecd305da1648b
Sam <shaughn.chan@gmail.com>
2024-09-11 19:42:36 -0400

    add script to dim external display

---
+++ hypr/dimdisplay.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+ext_display_connected=false
+if hyprctl monitors | grep "DP-1" > /dev/null; then
+    ext_display_connected=true
+fi
+
+case $1 in
+-d)
+    action="dim"
+;;
+
+-r)
+    action="reset"
+;;
+esac
+
+dimbrightness=50
+fullbrightness=90
+
+if [[ "$action" == "dim" ]]; then
+    brightnessctl -s set 10 > /dev/null
+    $ext_display_connected && ddcutil setvcp 10 $dimbrightness
+elif [[ "$action" == "reset" ]]; then
+    brightnessctl -r > /dev/null
+    $ext_display_connected && ddcutil setvcp 10 $fullbrightness
+fi
