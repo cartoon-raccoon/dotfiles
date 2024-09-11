3947a3dea7fd0f102e0edcc043cc77af643c68fe
Sam <shaughn.chan@gmail.com>
2024-09-11 19:19:32 -0400

    gammastep: lowered nighttime gamma

--- gammastep/gammastep.sh
+++ gammastep/gammastep.sh
@@ -11,4 +11,4 @@ ping -c 1 -W 50 1.1.1.1 || (notify-send "gammastep: unable to connect to network
 IFS="," read lat long <<< $(curl --silent https://ipinfo.io | jq .loc | tr -d '"')
 
 # launch the system tray version
-gammastep-indicator -l $lat:$long || notify-send "gammastep failed to launch"
+gammastep-indicator -l $lat:$long -t 6500:3500 || notify-send "gammastep failed to launch"
